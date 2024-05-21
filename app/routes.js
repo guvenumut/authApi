6ebc80b8dfadb11239ef8de4c01ca44de2894b61
guvenumut <umutgk6161@gmail.com>
2024-05-22 00:40:19 +0300

    eklemeler yapildi

--- app/routes.js
+++ app/routes.js
@@ -2,39 +2,32 @@ import express from 'express';
 import User from '../db/userSchema.js';
 import isEmail from '../utils/emailControl.js'
 import handleErrors from '../utils/handleErrors.js'
+import jwt from "jsonwebtoken"
+
 const router = express.Router();
 
 
 
 router.get("/",(req,res)=>{
-    res.status(200).json({message:"Ana Sayfa"})
+    res.render("home.ejs")
 })
 
-router.get('/register',(req,res)=>{
-  
+
+router.get("/signup",(req,res)=>{
+  res.render('signup');
 })
 
-router.post('/api/register', async (req, res) => {
+router.post('/signup', async (req, res) => {
     console.log('selamlar');
+    console.log(req.body.email,req.body.password)
     const { email, password } = req.body;
     try {
       const user = await User.create({email,password})
-      res.status(201).json(user);
+      const token=createToken(user._id)
+      res.cookie('jwt',token,{httpOnly:true,maxAge:maxAge*1000});
+      res.status(201).json({user:user._id});
       
-
-
-      
-    
-    
-    
-  
-      
-      
-
-      
-
-      
-     
+                       
       
     } catch (err) {
       const errors = handleErrors(err)
@@ -42,5 +35,16 @@ router.post('/api/register', async (req, res) => {
     }
   });
 
+
+  router.get('/login',(req,res)=>{
+    res.render('login')
+  })
+
+  const maxAge=3*24*60*60;
+  const createToken =(id)=>{
+    return jwt.sign({id},process.env.JWT_SECRET,{
+      expiresIn:maxAge
+    })
+  }
 
 export default router
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import router from './app/routes.js'
 import connectDatabase from './db/connectDB.js';
-
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -15,13 +15,19 @@ const app = express();
    
 
 
-
+app.use(express.static('public'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+
+
+app.set('view engine', 'ejs');
+
+
 app.use("/",router)
 
-
-
-
+app.get('/smoothies',(req,res)=>res.render("smoothies"));
 
 
 const PORT = process.env.PORT ;
